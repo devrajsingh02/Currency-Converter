e0f34f6f02759ac389c0f9599148c2437fc3222c
devrajsingh02 <devrajsolanki@gmail.com>
2026-01-18 16:50:28 +0530

    Added API and conversion logic

--- script.js
+++ script.js
@@ -4,13 +4,36 @@ let toCurrency=document.getElementById("tocurrency");
 let convertBtn=document.getElementById("convertbtn");
 let result=document.getElementById("result");
 
-convertBtn.addEventListener("click", ()=>{
+convertBtn.addEventListener("click", async ()=>{
     const amount=inputAmount.value;
     const from=fromCurrency.value;
     const to=toCurrency.value;
 
-    const rate=83;
-    const convertedAmount=amount*rate;
+    if(amount==="" || amount<=0){
+        result.textContent="Please enter a valid amount";
+        return;
+    }
 
-    result.textContent=`${amount} ${from} = ${convertedAmount} ${to}`;
-})
+    result.textContent="Converting...";
+
+    try{
+        const res = await fetch(
+            `https://api.exchangerate-api.com/v4/latest/${from}`
+        );
+
+        const data=await res.json();
+        console.log(data);
+
+        const rate= data.rates[to];
+
+        const converted = amount*rate;
+
+        result.textContent=`${from} ${amount} = ${converted} ${to}`;
+    }
+    catch(err){
+    result.textContent="Error fetching data";
+    }
+
+
+    
+});
